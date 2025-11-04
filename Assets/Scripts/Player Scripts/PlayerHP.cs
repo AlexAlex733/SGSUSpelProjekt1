4f6366b35cb1d0148d77145b6dbf4cb7aa392088
AlexAlex733 <alexander.phetsai@elev.ga.lbs.se>
2025-11-04 10:40:44 +0100

    Commit Placeholder playerHP UI
    Added a health tracker to the top left of the screen. And there are test health and heals incase you need to test. O for damage, P for heal.

--- Assets/Scripts/Player Scripts/PlayerHP.cs
+++ Assets/Scripts/Player Scripts/PlayerHP.cs
@@ -3,16 +3,58 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHP : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int playerHP;
+    public int playerMaxHP;
+    public int damage;
+    [SerializeField] KeyCode testDamage = KeyCode.O;
+    [SerializeField] KeyCode testHeal = KeyCode.P;
+
+    [SerializeField] GameObject heart1;
+    [SerializeField] GameObject heart2;
+    [SerializeField] GameObject heart3;
+  
     void Start()
     {
-        
+        playerHP = playerMaxHP;
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        if (Input.GetKeyDown(testDamage))
+        {
+            playerHP -= damage;
+        }
+        else if (Input.GetKeyDown(testHeal))
+        { 
+            playerHP += damage;
+        }
+
+        if (playerHP >= 3)
+        {
+            heart1.SetActive(true);
+            heart2.SetActive(true);
+            heart3.SetActive(true);
+        }
+        else if (playerHP == 2)
+        {
+            heart1.SetActive(true);
+            heart2.SetActive(true);
+            heart3.SetActive(false);
+
+        }
+        else if (playerHP == 1)
+        {
+            heart1.SetActive(true);
+            heart2.SetActive(false);
+            heart3.SetActive(false);
+        }
+        else if (playerHP <= 0)
+        {
+            heart1.SetActive(false);
+            heart2.SetActive(false);
+            heart3.SetActive(false);
+        }
     }
 
     public void Ondie ()
