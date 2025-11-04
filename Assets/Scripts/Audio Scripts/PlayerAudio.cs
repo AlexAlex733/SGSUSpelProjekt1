f564b2c815719d3037da6ced9d2a1c620d41ef6e
AlexAlex733 <alexander.phetsai@elev.ga.lbs.se>
2025-11-05 00:10:29 +0100

    Commit Main Protag, working on audio

--- Assets/Scripts/Audio Scripts/PlayerAudio.cs
+++ Assets/Scripts/Audio Scripts/PlayerAudio.cs
@@ -8,24 +8,46 @@ public class PlayerAudio : MonoBehaviour
     [SerializeField] KeyCode right = KeyCode.D;
     [SerializeField] KeyCode left = KeyCode.A;
     [SerializeField] KeyCode Jump = KeyCode.Space;
+    [SerializeField] bool isMoving;
+    [SerializeField] bool canPlaySound;
 
 
     private void Start()
     {
-        audioSource.loop = false;
+        isMoving = false;
         audioSource.clip = walkingSound;
+        audioSource.loop = true;
+        audioSource.PlayOneShot(walkingSound);
+        audioSource.Pause();
+        
     }
     private void Update()
     {
         if (Input.GetKey(right) || Input.GetKey(left))
         {           
-            audioSource.loop = true;
-            audioSource.Play();
+            isMoving = true;
         }
-       if (Input.GetKey(right))
+       else if (!Input.GetKeyUp(right) && !Input.GetKeyUp(left))
         {
-
+            isMoving= false;
+        }
+       if (isMoving || canPlaySound)
+        {
+          canPlaySound = false;
+          StartCoroutine(WalkingSoundCooldown());
+         
+        }
+       else if (!isMoving)
+        {
+            audioSource.Pause();
         }
 
+        IEnumerator WalkingSoundCooldown()
+        {
+            yield return new WaitForSeconds(0.7f);
+            audioSource.PlayOneShot(walkingSound);
+            canPlaySound = true;
+
+        }
     }
 }
