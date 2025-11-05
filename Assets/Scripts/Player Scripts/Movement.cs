2276f06cc94a77fc086b4642e94d5a3e02546a8d
AlexAlex733 <alexander.phetsai@elev.ga.lbs.se>
2025-11-05 12:57:51 +0100

    Commit, added dashing to protag

--- Assets/Scripts/Player Scripts/Movement.cs
+++ Assets/Scripts/Player Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.ComponentModel.Design;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -5,18 +6,28 @@ using UnityEngine.EventSystems;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] float dashCooldown = 1;
+    [SerializeField] bool canDash = true;
+    [SerializeField] bool isDashing = false;
+    [SerializeField] float dashTime = 0.2f;
+    [SerializeField, Range(0.0005f, 25)] float dashPower;
+
     private float horizontal;
+   
     public Transform groundCheckPoint; // Point from where the radius will be positioned
     public float groundCheckRadius = 0.2f; // Distance of the ground check
     public LayerMask groundMask; // Layer for ground objects
     [SerializeField] int jumpTimes = 2;
+    
     public bool isGrounded;
     public bool isFacingRight = true;
     [SerializeField, Range(0.0005f, 25)] float speed;
     [SerializeField, Range(0.0005f, 25)] float jumpForce;
+    
     [SerializeField] KeyCode right = KeyCode.D;
     [SerializeField] KeyCode left = KeyCode.A;
     [SerializeField] KeyCode Jump = KeyCode.Space;
+    [SerializeField] KeyCode dash = KeyCode.LeftShift;
     private Rigidbody2D rb;
 
 
@@ -29,6 +40,10 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
         horizontal = Input.GetAxisRaw("Horizontal");
         isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundMask);
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocityY);
@@ -37,12 +52,18 @@ public class Movement : MonoBehaviour
 
     private void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         Flip();
 
 
         if (isGrounded)
         {
             jumpTimes = 2;
+            canDash = true;
         }
 
         if (Input.GetKeyDown(Jump) && jumpTimes > 0)
@@ -53,6 +74,12 @@ public class Movement : MonoBehaviour
            
 
         }
+
+        if (Input.GetKeyDown(dash))
+        {
+            StartCoroutine(Dash());
+        }
+        
     }
 
     private void Flip()
@@ -64,5 +91,21 @@ public class Movement : MonoBehaviour
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+
+    IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.linearVelocity = new Vector2(transform.localScale.x * dashPower, 0f);
+        //tr.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+        //tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
