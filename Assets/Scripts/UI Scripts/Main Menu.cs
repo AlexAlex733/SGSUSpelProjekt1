9e394b462c3df90339865b12ebe9d65c2ce26f14
AlexAlex733 <alexander.phetsai@elev.ga.lbs.se>
2025-11-07 10:35:38 +0100

    Commit Main Menu

--- Assets/Scripts/UI Scripts/Main Menu.cs
+++ Assets/Scripts/UI Scripts/Main Menu.cs
@@ -1,13 +1,27 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] AudioSource buttonClick;
+    [SerializeField] AudioClip clickSound;
+
+    private void Start()
+    {
+        buttonClick.clip = clickSound;
+    }
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("LowerLevel");
+        buttonClick.Play();
+        StartCoroutine(clickWait());
+       
     }
 
-
+    IEnumerator clickWait()
+    {
+        yield return new WaitForSecondsRealtime(0.75f);
+        SceneManager.LoadSceneAsync("LowerLevel");
+    }
 
 }
